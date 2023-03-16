8a6faeb453acf4f52deb4c31eab4efea1fe17e7c
SangUn Park <Sangunpark1@gmail.com>
2023-03-16 01:35:20 -0400

    firebase dependencies and database added

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './Comments.css';
 import { AiOutlineUser } from "react-icons/ai";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { MdEdit } from "react-icons/md";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { v1 as generateUniqueID } from "uuid";
+import { db } from "../../firebase-config";
+import { collection, addDoc, serverTimestamp, onSnapshot, query, orderBy } from "firebase/firestore";
 
 export default function Comments({ allComments, setAllComments }) {
     const initialComment = {
@@ -12,33 +14,56 @@ export default function Comments({ allComments, setAllComments }) {
         text: ""
     }
 
-    const initialFocus = {
+    const initialInput = {
         commenter: false,
         text: false
     }
     
-    const [focus, setFocus] = useState(initialFocus);
+    const [validInput, setValidInput] = useState(initialInput);
     const [visibility, setVisibility] = useState(false);
     const [comment, setComment] = useState(initialComment);
     const [actionsToggle, setActionsToggle] = useState(false);
+    const commentsRef = collection(db, "comments");
+
 
     function handleInput(event) {
         setComment({...comment, [event.target.id]: event.target.value });
+        // Check
         if (event.target.value !== "") {
-            setFocus({...focus, [event.target.id]: true });
+            setValidInput({...validInput, [event.target.id]: true });
         } else {
-            setFocus({...focus, [event.target.id]: false });
+            setValidInput({...validInput, [event.target.id]: false });
         }
     }
 
-    function handleSubmit(event) {
+    useEffect(() => {
+        const queryComments = query(
+          commentsRef,
+          orderBy("createdAt")
+        );
+        const unsubscribe = onSnapshot(queryComments, (snapshot) => {
+          let comments = [];
+          snapshot.forEach((doc) => {
+            comments.push({ ...doc.data(), id: doc.id });
+          });
+          setComment(comments);
+        });
+    
+        return () => unsubscribe();
+      }, []);
+
+    const handleSubmit = async (event)  => {
         event.preventDefault();
-        if (!focus.commenter || !focus.text) {
+        if (!validInput.commenter || !validInput.text) {
             return
         }
+        await addDoc(commentsRef, {
+            text: comment,
+            createdAt: serverTimestamp()
+          });
         setAllComments([...allComments, comment]);
         setComment(initialComment);
-        setFocus(initialFocus);
+        setValidInput(initialInput);
     }
 
     function toggleComments(event) {
@@ -67,34 +92,34 @@ export default function Comments({ allComments, setAllComments }) {
                             value="Comment" 
                             id="submitComment"  
                             style={{
-                                background: (focus.text && focus.commenter) ? '#4caf50' : '#EFEFEF', 
-                                color: (focus.text && focus.commenter) ? 'white' : 'black', 
-                                cursor: (focus.text && focus.commenter) ? 'pointer' : 'auto'
+                                background: (validInput.text && validInput.commenter) ? '#4caf50' : '#EFEFEF', 
+                                color: (validInput.text && validInput.commenter) ? 'white' : 'black', 
+                                cursor: (validInput.text && validInput.commenter) ? 'pointer' : 'auto'
                             }}
                         />
                     </div>
-                }
-            </form>
-            <div>
-                {allComments.map(comment => {
-                    return (
-                        <div key={generateUniqueID()} className="commentsSectionDiv">
-                            <div className="commentsSection">
-                                <AiOutlineUser  className="userIcon" size={25} />
-                                <span className="commentsSection-commenter">{comment.commenter}</span>
-                                <li className="commentsSection-text">{comment.text}</li>
-                                <BiDotsVerticalRounded size={25} onClick={toggleActions} style={{cursor: 'pointer'}} className="commentsActionsToggle" />
-                            </div>
-                            {actionsToggle &&
-                                <div className="actionsDiv">
-                                    <span className="actionEdit"><MdEdit/>Edit</span>
-                                    <span className="actionDelete"><RiDeleteBin5Fill />Delete</span> 
+                                }
+                                </form>
+                                <div>
+                                    {allComments.map(comment => {
+                                        return (
+                                            <div key={generateUniqueID()} className="commentsSectionDiv">
+                                                <div className="commentsSection">
+                                                    <AiOutlineUser  className="userIcon" size={25} />
+                                                    <span className="commentsSection-commenter">{comment.commenter}</span>
+                                                    <li className="commentsSection-text">{comment.text}</li>
+                                                    <BiDotsVerticalRounded size={25} onClick={toggleActions} style={{cursor: 'pointer'}} className="commentsActionsToggle" />
+                                                </div>
+                                                {actionsToggle &&
+                                                    <div className="actionsDiv">
+                                                        <span className="actionEdit"><MdEdit/>Edit</span>
+                                                        <span className="actionDelete"><RiDeleteBin5Fill />Delete</span> 
+                                                    </div>
+                                                }
+                                            </div>
+                                        )
+                                    })}
                                 </div>
-                            }
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
+                            </div>
+                        )
+                    }
